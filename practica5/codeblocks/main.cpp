95bd07a0efd35d6e02fae07497c5e8706be2b072
Alexander <achicallag@unjbg.edu.pe>
2025-05-08 10:42:15 -0500

    prog4

--- practica5/codeblocks/main.cpp
+++ practica5/codeblocks/main.cpp
@@ -3,33 +3,31 @@
 
 using namespace std;
 
-bool s1 = false, s2 = false; // inicianlizandolas porsiacaso no toman el valor de falso
+int s1 = 0;
+int s2 = 0;
 
 void P1() {
     while (true) {
-        while (s2 == true) {
-
-        }
-        s1 = true;
-
-        cout << "Proceso 1 en su seccion critica" << endl;
-        s1 = false;
+        s1 = 1;
+        while (s2 == 1); // Espera activa
+        cout << "Proceso 1 esta en su seccion critica" << endl;
+        s1 = 0;
     }
 }
 
 void P2() {
     while (true) {
-        while (s1 == true) {
-
-        }
-        s2 = true;
-
-        cout << "Proceso 2 en su seccion critica" << endl;
-        s2 = false;
+        s2 = 1;
+        while (s1 == 1); // Espera activa
+        cout << "Proceso 2 esta en su seccion critica" << endl;
+        s2 = 0;
     }
 }
 
 int main() {
+    s1 = 0;
+    s2 = 0;
+
     thread P_1(P1);
     thread P_2(P2);
 
