f2611d45c01dfe7e0432a08e995e6460b93d7b05
Alexander <achicallag@unjbg.edu.pe>
2025-06-05 16:46:46 -0500

    añadiend archivos 1 y 2 cpp

---
+++ practica9/PrioridadEnLaLectura1.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<thread>
+#include <semaphore.h>
+using namespace std;
+sem_t mutex,wrt;
+int lec;
+void lector(){
+	while(true){
+		sem_wait(&mutex);
+		lec=lec+1;
+		if(lec==1){
+			sem_wait(&wrt);
+		}
+		sem_post(&mutex);
+		cout<<"Leer del recurso"<<endl;
+		sem_wait(&mutex);
+		lec=lec-1;
+		if(lec==0){
+			sem_post(&wrt);
+		}
+		sem_post(&mutex);
+	}		
+}
+void escritor(){
+	while(true){
+		sem_wait(&wrt);
+		cout<<"Escribir recurso"<<endl;
+		sem_post(&wrt);
+	}
+}
+
+int main(){
+	
+	
+	sem_init(&mutex,0,1);
+	//sem_init(&wrt,0,0);
+	thread le1(lector);
+	thread le2(lector);
+	thread le3(lector);
+	
+	thread es1(escritor);
+	thread es2(escritor);
+	thread es3(escritor);
+	
+	le1.join();
+	le1.join();
+	le1.join();
+	es1.join();
+	es2.join();
+	es3.join();
+
+
+	return 0;
+}
